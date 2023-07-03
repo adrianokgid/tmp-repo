4b5f821b74584755d3ea5ce86424135c79b64930
adrianokgid <48699641+adrianokgid@users.noreply.github.com>
2023-07-03 15:32:04 -0300

    Update demo.tf

--- demo.tf
+++ demo.tf
@@ -1,3 +1,10 @@
-module "demomodule" {
-  source = "https://github.com/adrianokgid/tmp-repo.git"
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIARYP22ZPP7AFJ3EDO"
+  secret_key = "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"
+}
+
+resource "aws_instance" "myEc2" {
+  ami = "ami-022e1a32d3f742bd8"
+  instance_type = "t2.micro"
 }
